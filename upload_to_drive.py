7ded5caddfcd37e5dece05e2e2034b16846b7f4f
Crisztina Zudor <Zudor.Ti.Crisztina@student.utcluj.ro>
2025-05-17 19:43:34 +0300

    Create upload_to_drive.py

---
+++ upload_to_drive.py
@@ -0,0 +1,18 @@
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseUpload
+from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
+import io
+
+def upload_pdf_to_drive(file_path, filename):
+    scope = ['https://www.googleapis.com/auth/drive.file']
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["general"], scope)
+
+    drive_service = build('drive', 'v3', credentials=creds)
+
+    with open(file_path, 'rb') as f:
+        file_metadata = {'name': filename, 'mimeType': 'application/pdf'}
+        media = MediaIoBaseUpload(f, mimetype='application/pdf')
+        file = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+
+    return file.get('id')  # returnează ID-ul fișierului
