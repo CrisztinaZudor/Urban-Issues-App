fe2f49ef8e169025ca582bf627d55518d289c518
Crisztina Zudor <Zudor.Ti.Crisztina@student.utcluj.ro>
2025-05-17 18:13:39 +0300

    Create save_to_google_sheets.py

---
+++ save_to_google_sheets.py
@@ -0,0 +1,23 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
+
+def save_to_google_sheets(report):
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
+    client = gspread.authorize(creds)
+    sheet = client.open("Raportari_Urbane").sheet1
+
+    row = [
+        report["timestamp"],
+        report["cracks"], report["fallen_trees"], report["graffiti"],
+        report["illegal_parking"], report["open_manhole"], report["overflowing_trashbin"],
+        report["pothole"], report["stray"], report["trash"], report["roadkills"], report["flood"],
+        report["broken_urban_furniture"], report["wild_animals"], report["dangerous_buildings"],
+        report["location"], report["description"], report["status"]
+    ]
+
+    sheet.append_row(row)
